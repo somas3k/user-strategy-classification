16dff498ec3cb4f02595a66f0457ccd9720d1dfb
somas3k <sikorka1901@gmail.com>
2020-01-21 14:03:14 +0100

    Some fixes to tree

--- tree.py
+++ tree.py
@@ -1,16 +1,20 @@
 from sklearn import tree
 import csv
+import matplotlib.pyplot as plt
 
 X = []
 Y = []
-field_names = ['tweets','retweets','average_hashtags','average_urls','average_followers','right_partiality','left_partiality','polarity','subjectivity','average_tweet_size']
+field_names = ['tweets', 'retweets', 'average_hashtags', 'average_urls', 'average_followers', 'right_partiality',
+               'left_partiality', 'polarity', 'subjectivity', 'average_tweet_size']
 with open('results.csv', newline='') as results:
     reader = csv.DictReader(results)
     for row in reader:
-      X.append([row[field_name] for field_name in field_names])
-      Y.append(row['label'])
+        X.append([row[field_name] for field_name in field_names])
+        Y.append(row['label'])
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
 
-tree.plot_tree(clf)
+plot = tree.plot_tree(clf, fontsize=3, proportion=True)
+plt.show()
+print()
