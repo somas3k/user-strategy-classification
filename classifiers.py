cfaf8f73bf45c2ef78d4068850d8da2eee92eb53
barkon <bpkonieczny@gmail.com>
2020-01-27 23:38:14 +0100

    removed stability check from classifiers.py, left it in tree.py

--- classifiers.py
+++ classifiers.py
@@ -4,35 +4,6 @@ import random
 import csv
 import matplotlib.pyplot as plt
 
-def check_stability(X, Y,file='resultsByQuarter.csv'):
-    with open(file, newline='') as samples:
-        Xs = []
-        Ys = []
-        user = None
-        date = []
-        clf = tree.DecisionTreeClassifier()
-        clf = clf.fit(X, Y)
-        reader = csv.DictReader(samples)
-        for row in reader:
-            if user is None:
-                user = row['user_id']
-            elif user != row['user_id']:
-                labels = clf.predict(Xs)
-                dates = date
-                plt.plot(dates, labels, 'ro')
-                plt.ylabel('labels')
-                plt.xlabel('dates')
-                plt.title(user)
-                plt.show()
-                Xs = []
-                Ys = []
-                date = []
-                user = row['user_id']
-
-            date.append(row['date'].split(" ")[0])
-            Xs.append([row[field_name] for field_name in field_names])
-            Ys.append(row['label'])
-
 X = []
 Y = []
 field_names = ['tweets', 'retweets', 'average_hashtags', 'average_urls', 'average_followers', 'right_partiality',
@@ -51,8 +22,6 @@ for i in range(int(len(X)*TEST_PERCENT_OF_SAMPLES)):
     rand_index = random.randint(0, len(X) - 1)
     Xs.append(X.pop(rand_index))
     Ys.append(Y.pop(rand_index))
-
-check_stability(X,Y)
 
 classifiers = {'decision tree': tree.DecisionTreeClassifier(), 'random forest': RandomForestClassifier(n_estimators=10)}
 accuracy = {}
